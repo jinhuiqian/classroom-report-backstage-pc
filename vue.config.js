a1cc685ddf72867c3514e5a3c08e11c48b8b3c8f
jinhuiqian <1286122850@qq.com>
2021-02-03 13:56:13 +0800

    first

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8888,
+    https: false,
+    open: true,
+    hotOnly: true
+  },
+  lintOnSave:false,
+  outputDir: './dist',
+  assetsDir: './assets/',
+  publicPath: './',
+  indexPath: './index.html',
+  transpileDependencies: ['vuetify']
+}
